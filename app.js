8953a5aa8eb4da1b44bc49d93d556e8f09fe357c
Ko-dany <iamgo910@gmail.com>
2022-04-07 21:24:24 +0900

    copied from kakao

---
+++ app.js
@@ -0,0 +1,7 @@
+// const mapContainer = document.getElementById("map");
+// const mapOption = {
+//     center: new kakao.maps.LatLng(33.450701, 126.570667),
+//     level: 3
+// };
+
+// const map = new kakao.maps.Map(mapContainer, mapOption); 
